c6a4de67316553d4929d909a1049ed8ac4202588
quandosale <fang.chp@gmail.com>
2017-06-22 16:11:22 +0800

    initial commit

---
+++ app/src/main/java/com/example/hero/ecgchart/ECGChart.java
@@ -0,0 +1,141 @@
+package com.example.hero.ecgchart;
+
+import android.app.Activity;
+import android.content.Context;
+import android.content.res.TypedArray;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.Point;
+import android.util.AttributeSet;
+import android.util.Log;
+import android.view.View;
+
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.Timer;
+import java.util.TimerTask;
+import java.util.Vector;
+import java.util.concurrent.BlockingDeque;
+import java.util.concurrent.LinkedBlockingDeque;
+
+/**
+ * Created by hero on 6/22/2017.
+ */
+
+public class ECGChart extends View {
+    final int SWEEP_MODE = 0;
+    final int FLOW_MODE = 1;
+    private int mLineColor;
+    private int mWindowSize;
+    private final int ONEWINDOW = 240;
+    private LinkedBlockingDeque<Integer> mInputBuf;
+    private Vector<Integer> mDrawingBuf;
+
+    private Paint mPaint;
+    private Paint mMaskBarPaint;
+    private int mDrawPosition;
+
+    private Activity mActivity;
+
+    private int mGraphMode = 1;
+
+    public ECGChart(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        mActivity = (Activity)context;
+        TypedArray a = context.getTheme().obtainStyledAttributes(
+                attrs,
+                R.styleable.ECGChart,
+                0, 0);
+
+        try {
+            mLineColor = a.getColor(R.styleable.ECGChart_lineColor, Color.WHITE);
+            mGraphMode = a.getColor(R.styleable.ECGChart_graphMode, SWEEP_MODE);
+            mWindowSize = a.getColor(R.styleable.ECGChart_windowSize, ONEWINDOW * 3);
+
+            mInputBuf = new LinkedBlockingDeque<>();
+            mDrawingBuf = new Vector<>();
+
+            mPaint = new Paint();
+            mPaint.setAntiAlias(true);
+            mPaint.setColor(mLineColor);
+            mMaskBarPaint = new Paint();
+            mMaskBarPaint.setColor(Color.rgb(0x33,0x33,0x33));
+            mMaskBarPaint.setStyle(Paint.Style.FILL);
+        } finally {
+            a.recycle();
+        }
+        init();
+    }
+
+    private void init() {
+        for(int i = 0 ; i < mWindowSize; i++)
+            mDrawingBuf.add(1250);
+
+        int redrawInterval = 50;
+        final int redrawPoints = ONEWINDOW / (1000/redrawInterval);
+        TimerTask drawEmitter = new TimerTask() {
+            @Override
+            public void run() {
+                mActivity.runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        if(mInputBuf.size() < ONEWINDOW)
+                            return;
+                        if(mGraphMode == SWEEP_MODE) {
+                            for(int i = 0 ; i < redrawPoints; i++) {
+                                int val = mInputBuf.pollFirst();
+                                mDrawingBuf.remove(mDrawPosition);
+                                mDrawingBuf.add(mDrawPosition ++, val);
+                                if(mDrawPosition >= mWindowSize) mDrawPosition = 0;
+                            }
+                        }
+                        else {
+                            for(int i = 0 ; i < redrawPoints; i++) {
+                                int val = mInputBuf.pollFirst();
+                                mDrawingBuf.remove(0);
+                                mDrawingBuf.add(val);
+                            }
+                        }
+
+                        invalidate();
+                        Log.d("inputBufSize", mInputBuf.size() + "");
+                    }
+                });
+            }
+        };
+        Timer timer = new Timer();
+        timer.schedule(drawEmitter, 0, redrawInterval);
+    }
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+        super.onDraw(canvas);
+
+        float width = this.getWidth();
+        float height = this.getHeight();
+
+        if(mDrawingBuf.size() < mWindowSize)
+            return;
+
+        float mapRatio = (float)width / mWindowSize;
+        int start = mDrawingBuf.get(0);
+        for(int i = 1 ; i < mWindowSize; i ++) {
+            int end = mDrawingBuf.get(i);
+            canvas.drawLine(i * mapRatio, start / 2500.f * height, (i + 1) * mapRatio, end / 2500.f * height , mPaint);
+            start = end;
+        }
+        if(mGraphMode == SWEEP_MODE)
+            canvas.drawRect((mDrawPosition-5) * mapRatio, 0, (mDrawPosition + 5) * mapRatio, height, mMaskBarPaint);
+    }
+    public void addEcgData(int[] data) {
+        checkBufOverflow();
+        for(int i = 0 ; i < data.length; i++) {
+            mInputBuf.addLast(data[i]);
+        }
+    }
+    public void checkBufOverflow() {
+        if(mInputBuf.size() > 2000)
+            mInputBuf.clear();
+    }
+}
---
+++ app/src/main/java/com/example/hero/ecgchart/MainActivity.java
@@ -0,0 +1,54 @@
+package com.example.hero.ecgchart;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+
+import java.util.Date;
+import java.util.Random;
+import java.util.Timer;
+import java.util.TimerTask;
+
+public class MainActivity extends AppCompatActivity {
+    private ECGChart mECGChart;
+
+    Random rand = new Random();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mECGChart = (ECGChart) findViewById(R.id.ecg_chart);
+        new Thread() {
+            public void run() {
+                int counter = 0;
+                while (counter++ < 1000) {
+                    try {
+                        rand.nextInt(1250);
+                        final double offset = 0.1;
+                        runOnUiThread(new Runnable() {
+
+                            @Override
+                            public void run() {
+                                int inputSize = 240;
+                                int[] ecgArray = new int[inputSize];
+                                double angle = 0;
+                                double interval = Math.PI / inputSize * 2;
+                                int randHeight = rand.nextInt(1250) % 1250;
+                                for(int i = 0; i < inputSize; i ++) {
+                                    ecgArray[i] = (int)(Math.sin(angle) * randHeight + 1250);
+                                    angle += interval;
+                                }
+                                mECGChart.addEcgData(ecgArray);
+                            }
+                        });
+                        Thread.sleep(1000);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+        }.start();
+    }
+}
